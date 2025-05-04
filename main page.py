4464e643b6638a5cca1d107903203ee441fce984
no609 <133532093+no609@users.noreply.github.com>
2025-05-04 23:37:20 +0530

    Update main page.py

--- main page.py
+++ main page.py
@@ -9,11 +9,19 @@ from firebase_admin import auth
 
 
 
-cred_dict = st.secrets["firebase"]
-cred = credentials.Certificate(cred_dict)
-
-# Initialize Firebase Admin SDK
 if not firebase_admin._apps:
+    cred = credentials.Certificate({
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"],
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+    })
     firebase_admin.initialize_app(cred)
 
 
